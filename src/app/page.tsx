02e887a2e3dcd137d3494b4b61f74f735522c532
detectivekang <kangseabich@naver.com>
2025-12-15 22:11:42 +0900

    pricing주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Hero from './Hero';
 import About from './About';
 import History from './History';
-import Pricing from './Pricing';
+// import Pricing from './Pricing';
 import Contact from './Contact';
 import Footer from './Footer';
 
