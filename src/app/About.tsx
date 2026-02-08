121817b2a06268acb4fa60227c0a1d36c9c9e394
detectivekang <126083968+detectivekang@users.noreply.github.com>
2026-02-08 20:50:04 +0900

    소개링크

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -14,20 +14,40 @@ export default function About() {
           <div className="col-lg-6 order-lg-1">
             <h2 className="fw-bold mb-3">인사말</h2>
             <p className="mb-3">
-              관악기 수리 전문가 전동헌은
-              일본에서 관악기 리페어와 색소폰 연주를 전공하며 유학하였고,
-              도쿄에서 연주 활동과 함께 리페어 공방을 운영했습니다.
-              2021년 귀국 후 충남 서산에 *관악기수리공방 제이디라보*를 오픈하였습니다.
-
-              TJB 대전방송에 관악기 수리 전문가로 소개된 바 있으며,
-              현재는 경복대학교(남양주캠퍼스) 색소폰리페어과에서
-              일본 학교에서 배운 커리큘럼과 다년간의 현장 경험을 바탕으로
-              색소폰 리페어뿐만 아니라 다양한 관악기 리페어 교육에 힘쓰고 있습니다.
+              관악기 수리 전문가 전동헌은 일본에서 관악기 리페어와 색소폰 연주를
+              전공하며 유학하였고, 도쿄에서 연주 활동과 함께 리페어 공방을
+              운영했습니다. 2021년 귀국 후 충남 서산에 *관악기수리공방
+              제이디라보*를 오픈하였습니다. TJB 대전방송에 관악기 수리 전문가로
+              소개된 바 있으며, 현재는 경복대학교(남양주캠퍼스)
+              색소폰리페어과에서 일본 학교에서 배운 커리큘럼과 다년간의 현장
+              경험을 바탕으로 색소폰 리페어뿐만 아니라 다양한 관악기 리페어
+              교육에 힘쓰고 있습니다.
             </p>
             <ul className="list-unstyled lh-lg">
-              <li>🎧 AI 기반 노이즈 프로파일 분석</li>
-              <li>🔧 오디오 복원 전문가 10년 경력</li>
-              <li>⚙️ 다양한 파일 포맷 완벽 지원</li>
+              <li>
+                <a
+                  href="https://www.youtube.com/watch?v=RQ8TuZLOtWQ&t=45s"
+                  target="_blank"
+                >
+                  📺TJB대전방송 관악기수리전문가 방송출연
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/watch?v=nKGDIoJD950&list=PLiwEetINyxjrgncSIjN-CIutjAOXHvsTM"
+                  target="_blank"
+                >
+                  🎷경복대학교 색소폰리페어과정 개강정보
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.youtube.com/watch?v=cFyYy201eL0&list=PLiwEetINyxjqXJ7tD49KFR-O6HlHdcvyR&index=13"
+                  target="_blank"
+                >
+                  🔧출장수리 Vlog 악기수리가 필요하면 어디든지갑니다!
+                </a>
+              </li>
             </ul>
           </div>
         </div>
